3258a3ff1683a5906ce029d9e4912c47f257a800
Ryan Erricson <ryanerricson@gmail.com>
2021-02-09 15:06:57 -0500

    commit tree 519109ba225f045f1a512bf702f3cc8deb34ebae

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[self.length - 1] != "."
+      false
+    else
+      true
+    end
   end
 
   def question?
-
+    if self[self.length-1] != "?"
+      false
+    else
+      true
+    end
   end
 
   def exclamation?
-
+    if self[self.length-1] != "!"
+      false
+    else
+      true
+    end
   end
 
   def count_sentences
-
+    newArray = self.split(/[\.!?] /)
+    newArray.length
   end
+
 end
